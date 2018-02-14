816453cbba183c63ca699c797251cea99be00824
Mark Brocato <mark.brocato@moovweb.com>
2018-02-14 16:46:26 -0500

    test components

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
+import MyFirstComponent from './MyFirstComponent'
+import MyStatelessComponent from './MyStatelessComponent'
 
 export default class Home extends Component {
+
+  state = {}
+
+  onTextChange = (e) => {
+    this.setState({ text: e.target.value })
+  }
+
   render() {
-    return <div>Home</div>
+    return (
+      <div>
+        <div>Home</div>
+        <MyFirstComponent message={this.state.text}/>
+        <MyStatelessComponent message={this.state.text}/>
+        <input type="text" onChange={this.onTextChange} value={this.state.text}/>
+      </div>
+    )
   }
 }
