2cdce3eac36dc518cdaea9b8f67f0da3ded0ce40
Mark Brocato <mark.brocato@moovweb.com>
2018-02-20 09:43:05 -0500

    menu

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { observer, inject } from "mobx-react"
 import Drawer from 'material-ui/Drawer'
 import List, { ListItem, ListItemText } from 'material-ui/List'
+import BackIcon from 'material-ui-icons/ArrowBack'
 import { Link } from 'react-router-dom'
 import AppBar from 'material-ui/AppBar'
 import Typography from 'material-ui/Typography'
@@ -9,6 +10,7 @@ import Toolbar from 'material-ui/Toolbar'
 import CloseIcon from 'material-ui-icons/Close'
 import IconButton from 'material-ui/IconButton'
 import styles from './Menu.module.scss'
+import ListItemIcon from 'material-ui/List/ListItemIcon';
 
 @inject('shop')
 @observer
@@ -41,8 +43,13 @@ export default class Menu extends Component {
             </List>
             { selected && (
               <div>
-                <div onClick={() => menu.goBack()}>{ selected.text }</div>
                 <List style={{minWidth: '250px'}}>
+                  <ListItem button onClick={() => menu.goBack()} classes={{ root: styles.header }}>
+                    <ListItemIcon style={{marginRight: 0}}>
+                      <BackIcon/>
+                    </ListItemIcon>
+                    <ListItemText classes={{ root: styles.headerText }} primary={<div className={styles.headerText}>{ selected.text } </div>}/>
+                  </ListItem>
                   { selected && selected.items.map((item, i) => (
                     <ListItem key={i} button component={Link} to={'/' + item.text.toLowerCase().replace(/\s/g, '-')}>
                       <ListItemText primary={item.text}/>
