5842ee47316564a917a7b695aed89e7b0202ba9a
Mark Brocato <mark.brocato@moovweb.com>
2018-02-21 08:40:39 -0500

    more features

--- pwa/src/category/SubcategoryItem.js
+++ pwa/src/category/SubcategoryItem.js
@@ -5,7 +5,7 @@ import styles from './SubcategoryItem.module.css'
 export default function SubcategoryItem({ subcategory }) {
   return (
     <li className={styles.subcategory}>
-      <Link to={`/subcategory/${subcategory.id}`}>
+      <Link to={`/c/${subcategory.id}?name=${encodeURIComponent(subcategory.name)}`}>
         <img src={subcategory.image}/>
         <div className={styles.name}>{subcategory.name}</div>
       </Link>
--- pwa/src/menu/Menu.js
+++ pwa/src/menu/Menu.js
@@ -24,15 +24,13 @@ export default class Menu extends Component {
     const width = 250;
 
     return (
-      <Drawer open={open} onClose={onClose} style={styles.drawer}>
-        <AppBar style={{ backgroundColor: 'white' }}>
+      <Drawer open={open} onClose={onClose} classes={{ paper: styles.drawer }}>
+        <AppBar className={styles.header}>
           <Toolbar>
-            <IconButton aria-label="Close Menu" onClick={onClose}>
+            <div style={{ flex: 1 }}/>
+            <IconButton aria-label="Close Menu" onClick={onClose} className={styles.button}>
               <CloseIcon />
             </IconButton>
-            <Typography type="title" color="inherit">
-              {title}
-            </Typography>
           </Toolbar>
         </AppBar>
         <div className={styles.menuWrap}>
@@ -42,15 +40,22 @@ export default class Menu extends Component {
                 <List style={{ width: `${width}px`, flex: 'none' }} key={i}>
                   {!item.root && (
                     <ListItem button onClick={() => menu.goBack()} classes={{ root: styles.header }}>
-                      <ListItemIcon style={{ marginRight: 0 }}>
-                        <BackIcon />
+                      <ListItemIcon>
+                        <BackIcon className={styles.back}/>
                       </ListItemIcon>
-                      <ListItemText classes={{ root: styles.headerText }} primary={<div className={styles.headerText}>{item.text} </div>} />
+                      <ListItemText 
+                        classes={{ root: styles.headerText }} 
+                        primary={<div className={styles.headerText}>{item.text} </div>} 
+                      />
                     </ListItem>
                   )}
                   {item.items && item.items.map((item, i) => (
                     <ListItem key={i} button onClick={this.onItemClick.bind(this, item, menu)}>
-                      <ListItemText primary={item.text} />
+                      <ListItemText 
+                        classes={{ root: styles.listItem }} 
+                        primary={item.text} 
+                        disableTypography 
+                      />
                     </ListItem>
                   ))}
                 </List>
@@ -62,11 +67,15 @@ export default class Menu extends Component {
     )
   }
 
+  componentWillMount() {
+    this.props.shop.menu.load()
+  }
+
   onItemClick = (item, menu) => {
-    const { history } = this.props;
+    const { history } = this.props
 
     if (item.url) {
-      history.push(item.url)
+      history.push(`${item.url}?name=${encodeURIComponent(item.text)}`)
     } else {
       menu.setSelected(item)
     }
