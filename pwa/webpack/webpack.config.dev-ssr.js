3a7b97ec69341824b91d0b0279ddb6440f7ec173
Mark Brocato <mark.brocato@moovweb.com>
2018-02-21 11:27:47 -0500

    sass modules

--- pwa/webpack/webpack.config.dev-ssr.js
+++ pwa/webpack/webpack.config.dev-ssr.js
@@ -3,11 +3,6 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const extractSass = new ExtractTextPlugin({
-  filename: "[name].[contenthash].css",
-  disable: process.env.NODE_ENV === "development"
-});
-
 module.exports = {
   name: 'SSR',
   context: path.join(__dirname, '..', 'src'),
@@ -24,18 +19,26 @@ module.exports = {
     loaders: commonLoaders.concat([
       {
         test: /\.scss$/,
-        use: extractSass.extract({
+        use: ExtractTextPlugin.extract({
           fallback: 'style-loader', 
-          use: [{
-            loader: "css-loader"
-          }, {
-            loader: "sass-loader"
-          }]
+          use: [
+            { 
+              loader: "css-loader" ,
+              query: {
+                localIdentName: '[hash:base64:5]',
+                modules: true
+              }
+            }, 
+            { loader: "sass-loader" }
+          ]
         })
       }
     ]),
   },
   plugins: [
-    extractSass
+    new ExtractTextPlugin({
+      filename: "[name].[contenthash].css", 
+      ignoreOrder: true
+    })
   ]
 };
