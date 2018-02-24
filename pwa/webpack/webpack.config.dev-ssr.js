1b75c556e3ff184f27c0326e34e885ff28f4f7c6
Mark Brocato <mark.brocato@moovweb.com>
2018-02-24 01:31:05 -0500

    postcss

--- pwa/webpack/webpack.config.dev-ssr.js
+++ pwa/webpack/webpack.config.dev-ssr.js
@@ -29,6 +29,34 @@ module.exports = {
                 modules: true
               }
             }, 
+            {
+              loader: require.resolve('css-loader'),
+              options: {
+                importLoaders: 1,
+                minimize: true,
+                sourceMap: true,
+              },
+            },
+            {
+              loader: require.resolve('postcss-loader'),
+              options: {
+                // Necessary for external CSS imports to work
+                // https://github.com/facebookincubator/create-react-app/issues/2677
+                ident: 'postcss',
+                plugins: () => [
+                  require('postcss-flexbugs-fixes'),
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9', // React doesn't support IE8 anyway
+                    ],
+                    flexbox: 'no-2009',
+                  }),
+                ],
+              },
+            },
             { loader: "sass-loader" }
           ]
         })
