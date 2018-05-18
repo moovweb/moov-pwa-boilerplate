207bd51a196100f48015ac7aee9c22e9d2799bdb
Mark Brocato <mark.brocato@moovweb.com>
2018-05-18 08:52:39 -0400

    Force SSL

--- scripts/moov_response_header_transform.js
+++ scripts/moov_response_header_transform.js
@@ -7,7 +7,19 @@ function removeCacheHeadersForVarnish() {
   headers.removeAllHeaders("Expires");
 }
 
+function redirectToHttps() {
+  headers.removeAllHeaders("Location");
+  headers.addHeader("Location", "https://" + env.host + env.path);
+  headers.statusCode = "301";
+  headers.statusText = "Moved Permanently";
+  headers.header("Cache-Control", "no-cache");
+}
+
 module.exports = function() {
+  if (env.secure === "false") {
+    return redirectToHttps();   
+  }
+
   if (env.MOOV_PWA_CACHE_CONTROL) {
     removeCacheHeadersForVarnish();
     headers.header("Cache-Control", env.MOOV_PWA_CACHE_CONTROL);
