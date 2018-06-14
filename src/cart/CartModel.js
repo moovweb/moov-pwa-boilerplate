ac5ffbe654f1cc18a926cb31fdd03c017576d6b3
Mark Brocato <mark.brocato@moovweb.com>
2018-06-14 14:48:55 -0400

    Fixed add to cart

--- src/cart/CartModel.js
+++ src/cart/CartModel.js
@@ -1,10 +1,11 @@
 import { types } from "mobx-state-tree"
 import CartModelBase from 'moov-pwa/model/CartModelBase'
+import ProductModel from '../product/ProductModel'
 import persist from 'moov-pwa/persist'
 
 const CartModel = types.compose(CartModelBase, types
   .model("CartModel", {
-    
+    items: types.optional(types.array(ProductModel), [])
   })
   .actions(self => ({
     afterCreate() {
