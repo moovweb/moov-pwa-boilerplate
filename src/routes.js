8de368c71758f2cc44e17c772ff99ef77beedfde
Mark Brocato <mark.brocato@moovweb.com>
2018-06-25 14:15:26 -0400

    Add page event tracking

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
-import { Router, fromClient, fromServer, cacheFor } from 'moov-pwa/router'
+import { Router, fromClient, fromServer, cacheFor, track } from 'moov-pwa/router'
+import analytics from 'moov-pwa/analytics'
 
 const cache = cacheFor({ duration: 300 }) // cache responses in varnish for 5 minutes
 
@@ -6,32 +7,38 @@ export default new Router()
   .get('/c/:id',
     fromClient({ page: 'Category' }),
     fromServer('./category/category-handler'),
+    track(analytics.categoryPageView),
     cache
   )
   .get('/s/:id',
     fromClient({ page: 'Subcategory' }),
     fromServer('./subcategory/subcategory-handler'),
+    track(analytics.subcategoryPageView),
     cache
   )
   .get('/p/:id',
     fromClient({ page: 'Product' }),
     fromServer('./product/product-handler'),
+    track(analytics.productPageView),
     cache
   )
   .get('/cart',
     fromClient({ page: 'Cart' }),
-    fromServer('./cart/cart-handler')
+    fromServer('./cart/cart-handler'),
+    track(analytics.cartPageView),
   )
   .get('/cart/add-from-amp.json',
     fromServer('./cart/add-from-amp-handler')
   )
   .get('/checkout', 
     fromClient({ page: 'Checkout' }),
-    fromServer('./checkout/checkout-handler')
+    fromServer('./checkout/checkout-handler'),
+    track(analytics.checkoutPageView)
   )
   .fallback(
     fromClient({ page: 'Home' }),
     fromServer('./home/home-handler'),
+    track(analytics.homePageView),
     cache
   )
     
