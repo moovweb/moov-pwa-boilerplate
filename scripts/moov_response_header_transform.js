8237e51eddbf4674a6b143a12bd84f537d59751e
Ivan Erceg <ivan.erceg@moovweb.com>
2018-07-25 16:29:45 -0400

    Reliably redirect on insecure connections

--- scripts/moov_response_header_transform.js
+++ scripts/moov_response_header_transform.js
@@ -30,7 +30,8 @@ function redirectToHttps() {
 }
 
 module.exports = function() {
-  if (env.secure === "false") {
+  // Always redirect on non-secure requests.
+  if (env.secure !== 'true') {
     return redirectToHttps();   
   }
 
