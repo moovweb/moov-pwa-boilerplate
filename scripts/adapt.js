74ce1b7865e587cd23af294d20e7bbe492de0e4b
Mark Brocato <mark.brocato@moovweb.com>
2018-08-17 07:01:33 -0700

    Update adapt.js

--- scripts/adapt.js
+++ scripts/adapt.js
@@ -5,7 +5,7 @@ export default async function adapt() {
   try {
     const stats = await getStats()
     fns.init$(body)
-    renderHeader(stats) // resuse the PWA header in adapt pages
+    renderHeader(stats) // reuse the PWA header in adapt pages
     sendResponse({ htmlparsed: true, body: $.html() })
   } catch (e) {
     sendResponse({ htmlparsed: true, body: e.stack })
