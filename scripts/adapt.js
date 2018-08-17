92702e8b146a616cf14c36523f01a8f47be9425b
Mark Brocato <mark.brocato@moovweb.com>
2018-08-17 07:03:13 -0700

    Update adapt.js

--- scripts/adapt.js
+++ scripts/adapt.js
@@ -1,6 +1,9 @@
 import renderHeader from '../src/adapt/renderHeader'
 import getStats from 'moov-pwa-stats'
 
+/**
+ * Runs for all URLs that don't match the PWA's router
+ */
 export default async function adapt() {
   try {
     const stats = await getStats()
