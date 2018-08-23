f1c6c767f5bc5c3e2d79cd0236f09f32a2e9824f
Mark Brocato <mark.brocato@moovweb.com>
2018-08-23 02:44:03 -0700

    remove console.log

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,6 @@ console.error = console.warn = console.log;
 const index = require('moov-pwa/platform').default
 
 module.exports = function() {
-  console.log('response rewriter')
-
   index({
     theme: require('../src/theme').default,
     model: require('../src/AppModel').default,
